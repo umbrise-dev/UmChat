cc0efd8ea546ae7a483d763688dfeb8f50ea6b3d
Xavier-Tse <xaviertse1024@gmail.com>
2025-03-31 19:28:18 +0800

    feat: ConversationList组件

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <template>
-  <h1 class="text-3xl font-bold bg-sky-300">💖 Hello World!</h1>
-  <p>Welcome to your Electron application.</p>
+  <div class="flex items-center justify-between h-screen">
+    <div class="w-[300px] bg-gray-200 h-full border-r border-gray-300">
+      <ConversationList :items="items" />
+    </div>
+    <div class="h-full flex-1">
+      content
+    </div>
+  </div>
 </template>
 
-<script setup>
-console.log('👋 This message is being logged by "renderer.ts", included via Vite');
+<script lang="ts" setup>
+import { ConversationProps } from "@/types"
+import ConversationList from "@/components/ConversationList.vue";
+
+const items: ConversationProps[] = [
+  { id: 1, selectedModel: 'GPT-3.5-Turbo', title: '你是一只猫娘1', createdAt: '2025-3-31', updatedAt: '2025-3-31', providerId: 1 },
+  { id: 2, selectedModel: 'GPT-3.5-Turbo', title: '你是一只猫娘2', createdAt: '2025-3-31', updatedAt: '2025-3-31', providerId: 1 },
+  { id: 3, selectedModel: 'GPT-3.5-Turbo', title: '你是一只猫娘3', createdAt: '2025-3-31', updatedAt: '2025-3-31', providerId: 1 },
+  { id: 4, selectedModel: 'GPT-3.5-Turbo', title: '你是一只猫娘4', createdAt: '2025-3-31', updatedAt: '2025-3-31', providerId: 1 },
+]
 </script>
--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -2,6 +2,7 @@ import type { ConfigEnv, UserConfig } from 'vite';
 import { defineConfig } from 'vite';
 import { pluginExposeRenderer } from './vite.base.config';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 // https://vitejs.dev/config
 export default defineConfig((env) => {
@@ -22,6 +23,9 @@ export default defineConfig((env) => {
     ],
     resolve: {
       preserveSymlinks: true,
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
     clearScreen: false,
   } as UserConfig;
