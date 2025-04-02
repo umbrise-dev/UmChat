9c57c26b2b24c4bb92fc9da767ef0d227e22d2fd
Xavier-Tse <xaviertse1024@gmail.com>
2025-04-02 11:42:08 +0800

    feat: MessageList组件

--- src/views/Conversation.vue
+++ src/views/Conversation.vue
@@ -1,7 +1,23 @@
 <template>
-  <h1>this is the conversation page</h1>
-</template>
+  <div class="w-[80%] mx-auto h-[85%] overflow-y-auto pt-2">
+    <MessageList :messages="messages"/>
+  </div>
+  <div class="w-[80%] mx-auto h-[15%] flex items-center">
+    <MessageInput  />
+  </div>
+  </template>
 
 <script lang="ts" setup>
+import MessageInput from '@/components/MessageInput.vue'
+import { MessageProps } from '../types'
+import MessageList from '@/components/MessageList.vue';
 
+const messages: MessageProps[] = [
+  { id: 1, content: '问题1', createdAt: '2024-07-03', updatedAt: '2024-07-03', type: 'question', conversationId: 1 },
+  { id: 2, content: '回答1', createdAt: '2024-07-03', updatedAt: '2024-07-03', type: 'answer', conversationId: 1 },
+  { id: 3, content: '问题2', createdAt: '2024-07-03',updatedAt: '2024-07-03',  type: 'question', conversationId: 1 },
+  { id: 4, content: '回答2', createdAt: '2024-07-03', updatedAt: '2024-07-03', type: 'answer', conversationId: 1 },
+  { id: 5, content: '问题3', createdAt: '2024-07-03', type: 'question', updatedAt: '2024-07-03', conversationId: 1 },
+  { id: 6, content: '', createdAt: '2024-07-03', updatedAt: '2024-07-03', type: 'answer', status: 'loading', conversationId: 1 },
+]
 </script>
