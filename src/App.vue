aee178e6a1f660f752f464ae68687d040432368e
Xavier-Tse <xaviertse1024@gmail.com>
2025-04-03 12:07:17 +0800

    refactor: 封装Button组件

--- src/App.vue
+++ src/App.vue
@@ -6,24 +6,14 @@
       </div>
       <div class="h-[10%] grid grid-cols-2 gap-2 p-2">
         <RouterLink to="/">
-          <button
-            class="shadow-sm inline-flex items-center justify-center
-              bg-green-700 text-white hover:bg-green-700/90 border border-green-700
-              h-[32px] py-[8px] px-[15px] text-sm rounded-[4px]"
-          >
-          <Icon icon="radix-icons:chat-bubble" class="mr-2"></Icon>
+          <Button icon-name="radix-icons:chat-bubble" class="w-full">
             新建聊天
-          </button>
+          </Button>
         </RouterLink>
         <RouterLink to="/settings">
-          <button
-            class="shadow-sm inline-flex items-center justify-center
-              bg-green-50 text-green-700 hover:bg-green-700 border border-green-700 hover:text-white
-              h-[32px] py-[8px] px-[15px] text-sm rounded-[4px]"
-          >
-          <Icon icon="radix-icons:gear" class="mr-2"></Icon>
+          <Button icon-name="radix-icons:gear" plain class="w-full">
             应用设置
-          </button>
+          </Button>
         </RouterLink>
       </div>
     </div>
@@ -34,7 +24,7 @@
 </template>
 
 <script lang="ts" setup>
-import { Icon } from "@iconify/vue"
 import ConversationList from "@/components/ConversationList.vue"
 import { conversations } from "@/testData";
+import Button from "./components/Button.vue";
 </script>
