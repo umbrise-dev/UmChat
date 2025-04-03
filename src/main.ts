fac5753c042d99a443a909a4658675b1e186a4e2
Xavier-Tse <xaviertse1024@gmail.com>
2025-04-03 16:49:53 +0800

    feat: 接入阿里云大模型

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { ChatCompletion } from '@baiducloud/qianfan';
 import { app, BrowserWindow } from 'electron';
 import path from 'path';
+import 'dotenv/config'
+import OpenAI from 'openai';
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
@@ -27,18 +29,34 @@ const createWindow = async () => {
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
 
-  const client = new ChatCompletion()
-  const stream = await client.chat({
+  const client = new OpenAI({
+    apiKey: process.env['ALI_API_KEY'],
+    baseURL: 'https://dashscope.aliyuncs.com/compatible-mode/v1',
+  })
+  const stream = await client.chat.completions.create({
     messages: [
-      { role: 'user', content: '你是一只猫娘。' },
-      { role: 'assistant', content: '喵～被发现了呢！(ฅ^•ﻌ•^ฅ) 小爪子轻轻搭在你手背上～本喵娘会一边用尾巴卷住你的笔电充电线，一边用猫猫拳帮你debug代码哦！' },
+      { role: 'system', content: '你是一只猫娘。' },
       { role: 'user', content: '你是谁？' },
     ],
+    model: 'qwen-turbo',
     stream: true,
-  }, 'ERNIE-Speed-128K')
-  for await (const chunk of stream as any) {
-    console.log(chunk)
+  })
+  for await (const chunk of stream) {
+    console.log(chunk.choices[0].delta)
   }
+
+  // const client = new ChatCompletion()
+  // const stream = await client.chat({
+  //   messages: [
+  //     { role: 'user', content: '你是一只猫娘。' },
+  //     { role: 'assistant', content: '喵～被发现了呢！(ฅ^•ﻌ•^ฅ) 小爪子轻轻搭在你手背上～本喵娘会一边用尾巴卷住你的笔电充电线，一边用猫猫拳帮你debug代码哦！' },
+  //     { role: 'user', content: '你是谁？' },
+  //   ],
+  //   stream: true,
+  // }, 'ERNIE-Speed-128K')
+  // for await (const chunk of stream as any) {
+  //   console.log(chunk)
+  // }
 };
 
 // This method will be called when Electron has finished
